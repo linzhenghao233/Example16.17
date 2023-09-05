99c6585818075c3e941a764d1b18e76c4baf6ec8
linzhenghao233 <1392872316@qq.com>
2023-09-05 22:05:28 +0800

    添加项目文件。

---
+++ qsorter.c
@@ -0,0 +1,12 @@
+/* qsorter.c -- 用qsort()排序一组数字 */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM 40
+void fillarray(double ar[], int n);
+void showarray(const double ar[], int n);
+int mycomp(const void* p1, const void* p2);
+
+int main(void) {
+
+}
